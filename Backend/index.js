98e50ac7de2ed1bdfcdb0253ffa9f9ad599bf86d
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-08-06 23:32:03 +0530

    updating index

--- Backend/index.js
+++ Backend/index.js
@@ -4,14 +4,22 @@ const inviteRoute = require('./Routes/BidInvitation')
 const RealTimeUpdateRoute = require('./Routes/RealTimeUpdate')
 const app = express()
 
+//Middleware 
 app.use(express.json())
 
+// Database Connection
+mongoose.connect('mongodb://localhost:27017/RealTimeBidEngine', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Routes
 app.use(actionRoute)
 
 app.use(inviteRoute)
 
 app.use(RealTimeUpdateRoute)
 
+
+
+// server is listening
 app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
