9afdb10ef7479a299e9253a2609ee1a91601d992
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-08-09 00:14:50 +0530

    updating config frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//    plugins: [react()],
+// })
+
+
+// -----------------
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-   plugins: [react()],
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'socket.io-client': require.resolve('socket.io-client')
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['socket.io-client']
+    }
+  }
+});
